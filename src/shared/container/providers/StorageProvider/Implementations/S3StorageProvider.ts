fe64f16d6318eb68eee441cdeb13c81501f7055e
LucasRios95 <lucashrios95@gmail.com>
2023-07-24 17:11:55 -0300

    upload no S3

--- src/shared/container/providers/StorageProvider/Implementations/S3StorageProvider.ts
+++ src/shared/container/providers/StorageProvider/Implementations/S3StorageProvider.ts
@@ -1,10 +1,38 @@
+import { S3 } from "aws-sdk";
+import fs from "fs";
+import mime from "mime";
+import { resolve } from "path";
+
+import upload from "@config/upload";
 import { IStorageProvider } from "../IStorageProvider";
 
 class S3StorageProvider implements IStorageProvider {
-    save(file: string): Promise<string> {
-        throw new Error("Method not implemented.");
+    private client: S3;
+
+    constructor() {
+        this.client = new S3({
+            region: process.env.AWS_BUCKET_REGION,
+
+        });
     }
-    delete(file: string): Promise<void> {
+
+    async save(file: string, folder: string): Promise<string> {
+        const originalName = resolve(upload.tmpFolder, file);
+
+        const fileContent = await fs.promises.readFile(originalName);
+
+        const ContentType = mime.getType(originalName);
+
+        await this.client.putObject({
+            Bucket: `${process.env.AWS_BUCKET}/${folder}`,
+            Key: file,
+            ACL: "public-read",
+            Body: fileContent,
+            ContentType
+        })
+
+    }
+    async delete(file: string, folder: string): Promise<void> {
         throw new Error("Method not implemented.");
     }
 }
